2d9045db8bdcde8fb253c53c568f64fcfa132fa8
linnn-7 <linbinjulie@gmail.com>
2022-06-11 19:14:21 +0800

    Create Fitness Page Title

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as SQLite from 'expo-sqlite';
+
+function FitnessScreen(){
+  return <View style={styles.container}>
+    <Text>Fitness Statistics</Text>
+    </View>
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Fitness" 
+        component={FitnessScreen} 
+        options={{
+          title:"Fitness Statistics",
+          headerTitleStyle:{
+            fontWeight:"bold",
+            fontSize:24,
+          },
+          headerStyle:{
+            backgroundColor:"orange",
+            height:120,
+            borderBottomColor:"#999",
+            borderBottomWidth:2,
+          },
+        }
+         }/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc',
     alignItems: 'center',
     justifyContent: 'center',
   },
