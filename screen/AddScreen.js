32da560b00c7472731a4db8a0b53d13369659d54
linnn-7 <linbinjulie@gmail.com>
2022-06-12 10:16:52 +0800

    Add 2 buttons

--- screen/AddScreen.js
+++ screen/AddScreen.js
@@ -1,21 +1,60 @@
 import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 
 export default function AddScreen ({ navigation }) {
     const [text, setText] = useState("PushUp");
     return(
       <View style={{ flex: 1, alignItems: 'center', justifyContent:'center'}}>
-        <Text>Add Your New Record!</Text>
-        <TextInput 
+        <Text style={styles.label}>Add Your New Record!</Text>
+        <TextInput style={styles.textInput}
             value={text}
             onChangeText={(newText) => setText(newText)}
         />
-        <TouchableOpacity 
-        onPress={() => navigation.goBack()} 
-        style={{ padding: 10}} 
-        >
-         <Text style={{ color: "orange" }}>Dismiss</Text>
-        </TouchableOpacity>
+        <View style={styles.buttons}>
+            <TouchableOpacity style={[styles.button, styles.submitButton]} onPress={() => navigation.goBack()}>
+                <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, styles.cancelButton]} onPress={() => navigation.goBack()}>
+                <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
+        </View>
+        <Text style={{ marginTop: 40, color: "grey" }}>
+            This is what you recorded:
+        </Text>
+        <Text style={{ color:"#333", marginTop: 10 }}>{text}</Text>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+      label:{
+          fontWeight: "bold",
+          fontSize: 24,
+      },
+      textInput: {
+          margin: 20,
+          borderWidth: 1,
+          width: "80%",
+          padding: 10,
+          borderColor: "#ccc",
+      },
+      buttons: {
+          flexDirection: "row",
+      },
+      button: {
+          padding: 10,
+          margin: 5,
+      },
+      buttonText: {
+          fontWeight: "bold",
+          color: "white",
+      },
+      submitButton: {
+          backgroundColor: "orange",
+      },
+      cancelButton: {
+          backgroundColor: "red",
+      },
+
+  }
+  )
